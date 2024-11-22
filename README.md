993c151872602b5e44e02e3ebe34a862b3cfebff
Ayman <ayman2002rahman@gmail.com>
2024-11-22 00:05:42 -0600

    Made some changes for playback

---
+++ README.md
@@ -0,0 +1,4 @@
+# MegaMiner Framework
+
+## Game Records
+All game records are recorded in here with the format of 
---
+++ playback.py
@@ -0,0 +1,5 @@
+# this file simulates a past game given its txt file
+file_path = input('Enter the file name of the recorded game: ')
+f = open('', "w")
+
+f.close()
